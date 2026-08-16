d43829d388e58b1cf43c43257277fb989df702b7
Muhammad Kashif Khan <kashifshahid394@gmail.com>
2026-08-16 20:42:52 +0500

    Create simple_calculator.py

---
+++ 01_calculator/simple_calculator.py
@@ -0,0 +1,18 @@
+print("===== DMAS CALCULATOR =====")
+
+# Take two numbers from the user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number (not zero): "))
+
+#Perform calculations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Display the results
+print("\n===== RESULTS =====")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
